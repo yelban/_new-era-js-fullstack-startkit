caf9a0eac7a711003a57e3d0ea779db64b97970d
Yelban <yelban@gmail.com>
2021-11-18 13:55:05 +0800

    Set Up the Application part 1/2

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,8 +2,10 @@
 // import "the-new-css-reset";
 // import "../css/index.less";
 import "../css/index.scss";
+import todoData from "./todo.json";
 
 console.log("index.js");
+console.log("todoData", todoData);
 
 // <script src="./js/index.js" async defer>
 // async after 'DOMContentLoaded'
@@ -12,6 +14,21 @@ console.log("index.js");
 //     console.log('ready');
 // });
 
+function renderTodos(todos) {
+  const arrRenderItem = todos.map((todo) => {
+    const classStatus = todo.done ? "done" : "";
+    return `
+      <li data-id="${todo.id}" class="${classStatus}">
+        <span class="custom-checkbox"></span>
+        <label>${todo.title}</label>
+      </li>
+    `;
+  });
+  document.querySelector(".todo-list").innerHTML = arrRenderItem.join("");
+}
+
 window.addEventListener("load", (event) => {
-  console.log("load");
+  console.log("load", event);
+
+  renderTodos(todoData);
 });
